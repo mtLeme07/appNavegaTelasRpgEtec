c8df72f0c87b6cb4322898fa3c8d4479b1ce3cea
3º MTEC - Desenv. de Sistemas <ds-mtec-3@servidor.acad>
2025-04-08 11:10:37 -0300

    -

--- App.js
+++ App.js
@@ -1,12 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './src/telas/Home';
+import Ficha from './src/telas/Ficha';
+import Jogo from './src/telas/Jogo';
+import Login from './src/telas/Login';
+
+import { Ionicons } from '@expo/vector-icons';
+const Tab = createBottomTabNavigator();
+
+function Tabs(){
+  return(
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        color= '#90d'
+        size = 30
+        if (route.name === 'Ficha') {
+          iconName = focused ? 'copy' : 'copy-outline';
+        }else if (route.name === 'Jogo') {
+          iconName = focused ? 'game-controller' : 'game-controller-outline'; 
+        }
+        
+        //aqui define os ícones que irão aparecer nas Tabs
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+    tabBarOptions={{
+      labelStyle: {
+        fontSize: 12},
+      activeTintColor: '#3f64c7',
+      inactiveTintColor: 'gray',      
+    }}    
+    >
+      <Tab.Screen name= "Ficha" component={Ficha}></Tab.Screen>
+      <Tab.Screen name= "Jogo" component={Jogo}></Tab.Screen>
+    </Tab.Navigator>
+  )
+}
+
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+ <NavigationContainer>
+ <Stack.Navigator initialRouteName='Login'>
+
+   <Stack.Screen 
+       name="Ficha" 
+       component={Tabs}
+       options={{
+         headerShown: false         
+       }}
+       >
+
+   </Stack.Screen>
+   <Stack.Screen name="Login" component={Login} options={{headerShown: false}}></Stack.Screen>
+   <Stack.Screen name="Home" component={Home} ></Stack.Screen>      
+   <Stack.Screen name="Jogo" component={Jogo} ></Stack.Screen>
+ </Stack.Navigator>
+</NavigationContainer>
   );
 }
 
